0bda1d218d318b07db1073c874f2bc520841cefc
taylorgrant <taylorgrant38@gmail.com>
2021-11-07 19:44:49 -0800

    specifying location for udpipe

--- R/run_btm.R
+++ R/run_btm.R
@@ -27,8 +27,9 @@ run_btm <- function(handle, data_source = c("timeline", "mentions"), n_topics, n
   d <- file.path(here::here(), "account_analysis", fs::path_sanitize(handle))
   d2 <<- file.path(d, as.character(Sys.Date()))
   # load udpipe model for POS tagging
-  ud_model <- udpipe::udpipe_download_model(language = "english",
+  udpipe::udpipe_download_model(language = "english",
                                             model_dir = aa)
+  ud_model <- udpipe_load_model(list.files(path = aa, pattern = "*.udpipe", full.names = TRUE))
   f <- file.path(d2, glue::glue("{handle}_twitter_info.rds"))
 
   if (data_source == "timeline") {
