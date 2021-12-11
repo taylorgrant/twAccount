df97fe9e12c665731e1014a28cde057e3e48757f
taylorgrant <taylorgrant38@gmail.com>
2021-12-11 11:08:21 -0800

    new method to expand urls

--- R/utils.R
+++ R/utils.R
@@ -32,25 +32,31 @@ decode_links <- function(tbl) {
   # function
   cat(crayon::cyan("Decoding links used on Twitter...\n"))
 
-  decode_url <- purrr::possibly(function(shortlink){httr::GET(shortlink)$url},
-                         otherwise = NA)
+  # decode_url <- purrr::possibly(function(shortlink){httr::GET(shortlink)$url},
+  #                        otherwise = NA)
   urls <- tbl %>%
     dplyr::select(urls_expanded_url) %>%
     tidyr::unnest(urls_expanded_url) %>%
     dplyr::filter(!is.na(urls_expanded_url)) %>%
-    dplyr::filter(!grepl("pbs\\.twimg\\.com|twitter\\.com|t\\.co", urls_expanded_url))
+    dplyr::filter(!grepl("pbs\\.twimg\\.com|twitter\\.com|t\\.co", urls_expanded_url)) %>%
+    dplyr::group_by(url = urls_expanded_url) %>%
+    dplyr::summarise(n = n())
 
   shorts <- urls %>%
-    dplyr::filter(!stringr::str_detect(urls_expanded_url, ".com")) %>%
-    dplyr::pull()
-  longs <- urls %>%
-    dplyr::filter(stringr::str_detect(urls_expanded_url, ".com"))
+    dplyr::filter(!stringr::str_detect(url, ".com")) %>%
+    dplyr::pull(url)
 
   su1 <- shorts[1:round((length(shorts)/2))]
   su2 <- shorts[(round((length(shorts)/2))+1):length(shorts)]
   cores <- parallel::detectCores(logical = TRUE) - 2
-  decode1 <- parallel::mclapply(su1, decode_url, mc.cores = cores)
-  decode2 <- parallel::mclapply(su2, decode_url, mc.cores = cores)
+  exp1 <- parallel::mclapply(su1, longurl::expand_urls, mc.cores = cores) %>%
+    purrr::map(~ dplyr::select(., -status_code)) %>% purrr::reduce(dplyr::bind_rows)
+  exp2 <- parallel::mclapply(su2, longurl::expand_urls, mc.cores = cores) %>%
+    purrr::map(~ dplyr::select(., -status_code)) %>% purrr::reduce(dplyr::bind_rows)
+  exp <- bind_rows(exp1,exp2)
 
-  links <- tibble::tibble(urls = unlist(c(longs, decode1, decode2)))
+  links <- urls %>%
+    dplyr::left_join(exp, by = c("url" = "orig_url")) %>%
+    dplyr::mutate(expanded_url = dplyr::case_when(stringr::str_detect(url, ".com") ~ url,
+                                                  TRUE ~ expanded_url))
 }
