802b7bae000bbda2aebd5378055db5b57d577504
taylorgrant <taylorgrant38@gmail.com>
2022-01-13 19:09:08 -0800

    adding url check

--- R/get_pics.R
+++ R/get_pics.R
@@ -14,11 +14,26 @@
 #' pics <- get_pics(d2, tl)
 #' }
 get_pics <- function(dir, tbl) {
-  tmp <- tbl %>%
+
+  # test urls (this is slow)
+  valid_url <- function(url_in,t=2){
+    con <- url(url_in)
+    check <- suppressWarnings(try(open.connection(con,open="rt",timeout=t),silent=T)[1])
+    suppressWarnings(try(close.connection(con),silent=T))
+    ifelse(is.null(check),TRUE,FALSE)
+  }
+
+  tmp <- tl %>%
     dplyr::filter(!is.na(media_url)) %>%
     dplyr::select(status_id, ext_media_url, created_at) %>%
     dplyr::mutate(year = lubridate::year(created_at)) %>%
     dplyr::arrange(year, created_at) %>%
+    tidyr::unnest(ext_media_url) %>%
+    dplyr::mutate(exist = sapply(ext_media_url, valid_url)) %>%
+    dplyr::filter(exist == "TRUE") %>%
+    dplyr::group_by(status_id, year) %>%
+    summarise(ext_media_url = list(ext_media_url)) %>%
+    dplyr::ungroup() %>%
     dplyr::group_by(year) %>%
     dplyr::mutate(index = dplyr::row_number(),
                   index = stringr::str_pad(index, 2, pad = "0"),
